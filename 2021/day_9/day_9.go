0bbd7905d2f1c4e2e79ee414952727550a092dfb
Lisette Anink <lisette@nerdasaservice.nl>
2021-12-09 16:55:48 +0100

    [wip] part 2

--- 2021/day_9/day_9.go
+++ 2021/day_9/day_9.go
@@ -54,10 +54,84 @@ func findLowPoints(floormap map[[2]int]int) int {
 }
 
 func findBasin(floormap map[[2]int]int) {
-	// for pos range floormap
+	for pos, val := range floormap {
+		checkBasin(floormap, pos)
+	}
 	// check if 9 skip
 	// check in basin
 	// no? put in basin /new
 	// check neighbours recursive
 	// del from floormap
+}
+
+func checkBasinNeighbours(floormap map[[2]int]int, pos [2]int, basinNr int) {
+	neighLeft := [2]int{pos[0] - 1, pos[1]}
+	if val, ok := floormap[neighLeft]; ok {
+		if val != 9 {
+			addToBasin(neighLeft, val, basinNr)
+			checkBasin(floormap, neighLeft)
+		}
+	}
+	neighRight := [2]int{pos[0] + 1, pos[1]}
+	if val, ok := floormap[neighRight]; ok {
+		if val != 9 {
+			addToBasin(neighRight, val, basinNr)
+			checkBasin(floormap, neighRight)
+		}
+	}
+	neighTop := [2]int{pos[0], pos[1] - 1}
+	if val, ok := floormap[neighTop]; ok {
+		if val != 9 {
+			addToBasin(neighTop, val, basinNr)
+			checkBasin(floormap, neighTop)
+		}
+	}
+	neighBottom := [2]int{pos[0], pos[1] + 1}
+	if val, ok := floormap[neighBottom]; ok {
+		if val != 9 {
+			addToBasin(neighBottom, val, basinNr)
+			checkBasin(floormap, neighBottom)
+		}
+	}
+}
+
+func checkBasin(floormap map[[2]int]int, pos [2]int) {
+	if floormap[pos] != 9 {
+		nr := findBasinNr(pos)
+		if nr == 0 {
+			addToBasin(pos, floormap[pos], nr)
+		} else {
+			checkBasinNeighbours(floormap, pos, nr)
+		}
+	}
+	delete(floormap, pos)
+}
+
+func addToBasin(pos [2]int, val, nr int) {
+	if nr != 0 {
+		basins[nr] = append(basins[nr], pos)
+	} else {
+		basins[len(basins)+1] = [][2]int{pos}
+	}
+}
+
+var basins = map[int][][2]int{}
+
+func findBasinNr(pos [2]int) int {
+	basinNr := 0
+	for x, b := range basins {
+		if contains(b, pos) {
+			basinNr = x
+		}
+	}
+	return basinNr
+}
+
+func contains(basin [][2]int, pos [2]int) bool {
+	for _, i := range basin {
+		if i == pos {
+			return true
+		}
+	}
+	return false
 }
