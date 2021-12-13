8219afdb9bd23cbcc75aae8f6cbf9b8098f43208
Lisette Anink <lisette.anink@nrds.eu>
2021-12-13 09:14:17 +0100

    [WIP] day 10 unsolved

--- 2021/day_10/day_10.go
+++ 2021/day_10/day_10.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"strings"
 
-	parsec "github.com/prataprc/goparsec"
+	"lisette.anink/aoc/utils"
 )
 
 // * And, to combine a sequence of terminals and non-terminal parsers.
@@ -13,74 +13,125 @@ import (
 // * Many, to repeat the parser one or more times.
 // * ManyUntil, to repeat the parser until a specified end matcher.
 // * Maybe, to apply the parser once or none.
-func defineParser(ast *parsec.AST) parsec.Parser {
-	var content parsec.Parser
+// func defineParser(ast *parsec.AST) parsec.Parser {
+// 	var content parsec.Parser
 
-	// terminal parsers.
-	tagbro := parsec.Atom("(", "OT")
-	tagbrc := parsec.Atom(")", "OT")
-	tagsqo := parsec.Atom("[", "CT")
-	tagsqc := parsec.Atom("]", "CT")
-	tagcuro := parsec.Atom("{", "CT")
-	tagcurcl := parsec.Atom("}", "CT")
-	tagsmt := parsec.Atom("<", "CT")
-	taggrt := parsec.Atom(">", "CT")
+// 	// terminal parsers.
+// 	tagbro := parsec.Atom("(", "OT")
+// 	tagbrc := parsec.Atom(")", "OT")
+// 	tagsqo := parsec.Atom("[", "CT")
+// 	tagsqc := parsec.Atom("]", "CT")
+// 	tagcuro := parsec.Atom("{", "CT")
+// 	tagcurcl := parsec.Atom("}", "CT")
+// 	tagsmt := parsec.Atom("<", "CT")
+// 	taggrt := parsec.Atom(">", "CT")
 
-	brackets := ast.And(
-		"brackets", nil, tagbro, &content, tagbrc,
-	)
-	// sqbrackets := ast.And(
-	// 	"sqbrackets", nil, tagsqo, &content, tagsqc,
-	// )
-	// cubrackets := ast.And(
-	// 	"curbrackets", nil, tagcuro, &content, tagcurcl,
-	// )
-	// arbrackets := ast.And(
-	// 	"arbrackets", nil, tagsmt, &content, taggrt,
-	// )
-	// content = ast.OrdChoice("content", nil, brackets, sqbrackets, cubrackets, arbrackets)
-	content = ast.OrdChoice("content", nil, tagsqo, tagsqc, tagcuro, tagcurcl, tagsmt, taggrt, brackets)
-	contents := ast.Maybe(
-		"maybecontents", nil, ast.Kleene("contents", nil, content, nil),
-	)
-	return ast.Kleene("c", nil, contents, nil)
-}
+// 	brackets := ast.And(
+// 		"brackets", nil, tagbro, &content, tagbrc,
+// 	)
+// 	// sqbrackets := ast.And(
+// 	// 	"sqbrackets", nil, tagsqo, &content, tagsqc,
+// 	// )
+// 	// cubrackets := ast.And(
+// 	// 	"curbrackets", nil, tagcuro, &content, tagcurcl,
+// 	// )
+// 	// arbrackets := ast.And(
+// 	// 	"arbrackets", nil, tagsmt, &content, taggrt,
+// 	// )
+// 	// content = ast.OrdChoice("content", nil, brackets, sqbrackets, cubrackets, arbrackets)
+// 	content = ast.OrdChoice("content", nil, tagsqo, tagsqc, tagcuro, tagcurcl, tagsmt, taggrt, brackets)
+// 	contents := ast.Maybe(
+// 		"maybecontents", nil, ast.Kleene("contents", nil, content, nil),
+// 	)
+// 	return ast.Kleene("c", nil, contents, nil)
+// }
 
-func parseLine(line string) {
-	ast := parsec.NewAST("line", 100)
-	y := defineParser(ast)
-	s := parsec.NewScanner([]byte(line))
-	node, _ := ast.Parsewith(y, s)
-	fmt.Println(node.GetValue())
-	ast.Prettyprint()
-	graph := ast.Dotstring("simplehtml")
-	fmt.Println(graph)
-	ch := make(chan parsec.Queryable, 100)
-	ast.Query(".term", ch)
-	for node := range ch {
-		fmt.Printf("%s", node.GetValue())
+// func parseLine(line string) {
+// 	ast := parsec.NewAST("line", 100)
+// 	y := defineParser(ast)
+// 	s := parsec.NewScanner([]byte(line))
+// 	node, _ := ast.Parsewith(y, s)
+// 	fmt.Println(node.GetValue())
+// 	ast.Prettyprint()
+// 	graph := ast.Dotstring("simplehtml")
+// 	fmt.Println(graph)
+// 	ch := make(chan parsec.Queryable, 100)
+// 	ast.Query(".term", ch)
+// 	for node := range ch {
+// 		fmt.Printf("%s", node.GetValue())
+// 	}
+// 	fmt.Println()
+// }
+func mapFromString(in string) map[int]string {
+	elements := strings.Split(in, "")
+	mapElements := map[int]string{}
+	for i, e := range elements {
+		mapElements[i] = e
 	}
-	fmt.Println()
-
+	return mapElements
 }
-
-func dumbWay(lines []string) {
+func dumbWay(lines []string) []string {
+	list := []string{}
 	for _, line := range lines {
 		if len(line) > 0 {
-			findCorrect(line)
+			elements := mapFromString(line)
+			if ok, wrong := findCorrect(elements, 0, 1); !ok {
+				fmt.Println("wrong", wrong, line)
+				list = append(list, wrong)
+			} else {
+				fmt.Println("right", line)
+
+			}
+
 		}
 	}
+	return list
 }
 
-func findCorrect(line string) {
-	elements := strings.Split(line, "")
-	for _, e := range elements {
-		switch e {
-		case "(":
-		case "<":
-		case "{":
-		case "[":
-
+// if open
+// look for closed
+// if first is right closed !top
+// als einde van map niet is bereikt open volgnede
+// elsif first is open dan
+// look for closed
+// elsif first is wrong
+// return false
+func findCorrect(elements map[int]string, current, close int) (bool, string) {
+	fmt.Println("start", current, close, elements[current])
+	if isOpen(elements[current]) {
+		// fmt.Println("isOpen", current, close)
+		if isClose(elements[current], elements[close]) {
+			fmt.Println("isClose", current, close)
+			return true, elements[current]
+		} else if isOpen(elements[close]) {
+			fmt.Println("isNewOpen", close, elements[close])
+			return findCorrect(elements, close, close+1)
+		} else {
+			fmt.Println("isWrong", close, elements[close])
+			return false, elements[current]
 		}
+	}
+	// fmt.Println("!isOpen", current, close)
+	return false, elements[current]
+}
+
+var allOpen = []string{"(", "<", "{", "["}
+
+func isOpen(current string) bool {
+	return utils.IncludesAll(allOpen, []string{current})
+}
+
+func isClose(open, close string) bool {
+	switch open {
+	case "(":
+		return close == ")"
+	case "<":
+		return close == ">"
+	case "{":
+		return close == "}"
+	case "[":
+		return close == "]"
+	default:
+		return false
 	}
 }
--- 2021/day_10/day_10_test.go
+++ 2021/day_10/day_10_test.go
@@ -39,7 +39,27 @@ func useInputData() []string {
 // 	}
 // }
 
-func Test_parseLine(t *testing.T) {
+// func Test_parseLine(t *testing.T) {
+// 	// lines := useTestData()
+// 	type args struct {
+// 		line string
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 	}{
+// 		{"line 1", args{"((}))"}},
+// 		{"line 1", args{"(({})[{}])"}},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			parseLine(tt.args.line)
+// 			assert.Equal(t, 3, 4)
+// 		})
+// 	}
+// }
+
+func Test_isOpen(t *testing.T) {
 	// lines := useTestData()
 	type args struct {
 		line string
@@ -47,14 +67,114 @@ func Test_parseLine(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
+		want bool
 	}{
-		{"line 1", args{"((}))"}},
-		{"line 1", args{"(({})[{}])"}},
+		{"line (", args{"("}, true},
+		{"line {", args{"{"}, true},
+		{"line }", args{"}"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			parseLine(tt.args.line)
-			assert.Equal(t, 3, 4)
+			got := isOpen(tt.args.line)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_isClose(t *testing.T) {
+	type args struct {
+		open  string
+		close string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"line (", args{"(", ")"}, true},
+		{"line {", args{"{", "}"}, true},
+		{"line {", args{"<", ">"}, true},
+		{"line {", args{"[", "]"}, true},
+		{"line }", args{"[", "}"}, false},
+		{"line }", args{"[", ")"}, false},
+		{"line }", args{"[", ">"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isClose(tt.args.open, tt.args.close); got != tt.want {
+				t.Errorf("isClose() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findCorrect(t *testing.T) {
+
+	type args struct {
+		elements map[int]string
+		current  int
+		close    int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  bool
+		want1 string
+	}{
+		{"small test", args{map[int]string{0: "(", 1: "{", 2: "}", 3: ")"}, 0, 1}, true, "{"},
+		{"small test fail", args{map[int]string{0: "(", 1: "{", 2: "}", 3: "[", 4: "}"}, 0, 1}, false, "]"},
+		{"bigger test true", args{map[int]string{0: "[", 1: "(", 2: "(", 3: ")", 4: "[", 5: "<", 6: ">", 7: "]", 8: ")", 9: "]", 10: "(", 11: "{", 12: "[", 13: "<", 14: "{", 15: "<", 16: "<", 17: "[", 18: "]", 19: ">", 20: ">", 21: "("}, 0, 1}, true, "("},
+		{"bigger test fail", args{map[int]string{0: "{", 1: "(", 2: "[", 3: "(", 4: "<", 5: "{", 6: "}", 7: "[", 8: "<", 9: ">", 10: "[", 11: "]", 12: "}", 13: ">", 14: "{", 15: "[", 16: "]", 17: "{", 18: "[", 19: "(", 20: "<", 21: "(", 22: ")", 23: ">"}, 0, 1}, false, "}"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := findCorrect(tt.args.elements, tt.args.current, tt.args.close)
+			if got != tt.want {
+				t.Errorf("findCorrect() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("findCorrect() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func Test_dumbWay(t *testing.T) {
+	test := useTestData()
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"test", args{test}, []string{")"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := dumbWay(tt.args.lines)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_mapFromString(t *testing.T) {
+	type args struct {
+		in string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[int]string
+	}{
+		{"test 1", args{"[(()[<>])]({[<{<<[]>>("}, map[int]string{0: "[", 1: "(", 2: "(", 3: ")", 4: "[", 5: "<", 6: ">", 7: "]", 8: ")", 9: "]", 10: "(", 11: "{", 12: "[", 13: "<", 14: "{", 15: "<", 16: "<", 17: "[", 18: "]", 19: ">", 20: ">", 21: "("}},
+		{"test 2", args{"{([(<{}[<>[]}>{[]{[(<()>"}, map[int]string{0: "{", 1: "(", 2: "[", 3: "(", 4: "<", 5: "{", 6: "}", 7: "[", 8: "<", 9: ">", 10: "[", 11: "]", 12: "}", 13: ">", 14: "{", 15: "[", 16: "]", 17: "{", 18: "[", 19: "(", 20: "<", 21: "(", 22: ")", 23: ">"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := mapFromString(tt.args.in)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
